d97bd126cfe3d1f35e6d7685ae2366366ffda026
Vybekid <felobitto@gmail.com>
2025-08-16 12:36:09 +0300

    progress

--- brace.py
+++ brace.py
@@ -1,28 +1,23 @@
-import turtle
-import colorsys
+from turtle import *
+import colorsys as cs
 
-screen = turtle.Screen()
-screen.bgcolor("black")
-screen.title("Geometric Spirograph (Slow and Centered)")
+bgcolor('black')
+tracer(10)
+pensize(4)
+h = 0
 
-t = turtle.Turtle()
-t.speed(0)
-t.width(2)
-
-h = 0.0
-
-for i in range(125):
-    c = colorsys.hsv_to_rgb(h, 1.0, 1.0)
-    t.pencolor(c)
-
-    t.forward(i) 
-    t.right(91)
-    t.circle(i / 2, 90) 
-    t.left(120)
-    t.forward(i / 2)
-    t.right(150)
-
-    h += 0.005
-
-t.hideturtle()
-screen.mainloop()
+for i in range(200):
+    c = cs.hsv_to_rgb(h, 1, 1)
+    h += 0.01
+    fillcolor(c)
+    pencolor('black')
+    begin_fill()
+    fd(i)
+    rt(67)
+    fd(i)
+    circle(i, 30)
+    fd(i)
+    lt(190)
+    end_fill()
+    
+done()
